b3ac98462879ba5520e1b0383ba04cc5add0d68d
Abed Hasan <abedhasan79@gmail.com>
2022-10-05 23:17:25 -0400

    added required packages and questions array in index.js

---
+++ index.js
@@ -0,0 +1,54 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+const fs = require('fs');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        message: 'Enter the title of your Project.',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Enter the Description of your Project.',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'Enter the installation instruction of your Project.',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'Enter the usage information of your Project.',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'Enter the license of your Project From the following list. MIT, BSL, Apache or none: ',
+        name: 'license'
+    },
+    {
+        type: 'input',
+        message: 'Enter the contributing guidline for your Project.',
+        name: 'contributing'
+    },
+    {
+        type: 'input',
+        message: 'Enter the test instruction of your Project.',
+        name: 'test'
+    },
+    {
+        type: 'input',
+        message: 'Enter your GitHub username.',
+        name: 'username'
+    },
+    {
+        type: 'input',
+        message: 'Enter your email',
+        name: 'email'
+    }
+
+];
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -36,3 +36,5 @@ function renderLicenseSection(license) {
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {}
+
+module.exports = generateMarkdown;
