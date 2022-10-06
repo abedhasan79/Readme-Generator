71d12a1c967f54158f587c6e24aaff68402ace12
Abed Hasan <abedhasan79@gmail.com>
2022-10-05 23:13:25 -0400

    added lisenceBadge and lisenceLink on generateMarkdownjs file

---
+++ utils/generateMarkdown.js
@@ -0,0 +1,26 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+    let badge = '';
+    if (license !== 'none') {
+        badge = `![license badge](https://img.shields.io/badge/license-${license}-success)`;
+    }
+    return badge;
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+    let link;
+    if (license === 'MIT') {
+        link = '[https://choosealicense.com/licenses/mit](https://choosealicense.com/licenses/mit)';
+    } else if (license === 'Apache') {
+        link = '[https://choosealicense.com/licenses/apache-2.0/](https://choosealicense.com/licenses/apache-2.0/)';
+    } else if (license === 'BSL') {
+        link = '[https://choosealicense.com/licenses/](https://choosealicense.com/licenses/)'
+    } else {
+        link = '';
+    }
+    return link;
+
+}
