9491ca1ccd14df67cfd8198627aafade6b49bc47
Abed Hasan <abedhasan79@gmail.com>
2022-10-05 23:14:59 -0400

    added lisneSectrion function in generateMarkdown file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -24,3 +24,15 @@ function renderLicenseLink(license) {
     return link;
 
 }
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+    let licenseSection = '';
+    if (license !== 'none') {
+        licenseSection = `## License\n${license}\n${renderLicenseLink(license)}\n`;
+    }
+    return licenseSection;
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {}
